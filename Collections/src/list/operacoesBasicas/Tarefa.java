663d80b9f80d85d4a029ceba4407cb22bf5c42a4
idemilsonufma01 <idemilson.silva@discente.ufma.br>
2024-06-05 21:12:04 -0300

    Resolvendo desafio ListaTarefa

---
+++ Collections/src/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package list.operacoesBasicas;
+
+public class Tarefa {
+  // atributo
+  private String descricao;
+
+  // construtor
+  public Tarefa(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public String toString() {
+    return descricao;
+  }
+}
